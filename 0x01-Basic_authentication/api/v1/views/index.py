48eb3a5f48ede7e773ae3c1f355f60d3cf9b1461
codam <damisco005@gmail.com>
2024-11-12 10:20:09 +0100

    added 401 handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def test_401():
+    """ Tests the unauthorized handler"""
+    abort(401)
