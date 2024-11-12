6f065f5806747c4106b045054d70c22f9d364984
codam <damisco005@gmail.com>
2024-11-12 18:16:36 +0100

    auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+auth module containing auth class
+"""
+from flask import request
+
+
+class Auth:
+    """API authentication class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ returns false"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization header """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Get currents user object"""
+        return None
