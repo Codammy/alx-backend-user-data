44491b14290671f7deb4494e7751ed9c1b8f3a23
codam <damisco005@gmail.com>
2024-11-14 04:57:50 +0100

    extracts authorization header value

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,4 +6,12 @@ from api.v1.auth import Auth
 
 class BasicAuth(Auth):
     """Basic authentication"""
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str)\
+            -> str:
+        """extracts base64 authorization header"""
+        if not authorization_header or type(authorization_header) is not str\
+                or not authorization_header.startswith('Basic'):
+            return None
+        login = authorization_header.strip()[6:]
+        return login
