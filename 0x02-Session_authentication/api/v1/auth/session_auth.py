c4bce0b835b84f0119f3465f894200862ba55edb
codam <damisco005@gmail.com>
2024-11-17 05:17:58 +0100

    user id for session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -14,10 +14,13 @@ class SessionAuth(Auth):
     def create_session(self, user_id: str = None)\
             -> str:
         """creates and store a user session using their id"""
-        if user_id is None:
+        if user_id is None or type(user_id) is not str:
             return None
-        if type(user_id) is not str:
-            return None
-        session_id = uuid.uuid4()
+        session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        if not session_id or type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
