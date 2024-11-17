a1e29038a06ecafdfc3336416f4bb4d3eec3cd65
codam <damisco005@gmail.com>
2024-11-17 05:06:24 +0100

    new commit

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,20 @@
 """
 
 from api.v1.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Session auth class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None)\
+            -> str:
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        session_id = uuid.uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
