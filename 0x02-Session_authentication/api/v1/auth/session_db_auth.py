719b9452e9100d363f70312f63b585ca7d41bd76
codam <damisco005@gmail.com>
2024-11-18 17:56:49 +0100

    new commit

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """
+SessionDBAuth
 """
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
@@ -8,6 +9,7 @@ from os import getenv
 
 class SessionDBAuth(SessionExpAuth):
     """
+    Class for SessionDBAuth
     """
     def create_session(self, user_id=None):
         """creates and stores new instance of
