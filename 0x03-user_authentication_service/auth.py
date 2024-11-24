7431025d2ee28d111c52c573dd4041284418ff12
codam <damisco005@gmail.com>
2024-11-25 00:54:39 +0100

    implemented create user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,9 +2,32 @@
 """user authentication module
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """encrypts str password and return bytes password"""
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode(), salt)
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+    def __init__(self):
+        """instantiate class"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """creates a new user
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            pass
+        if type(password) is not bytes:
+            password = _hash_password(password)
+        return self._db.add_user(email, password)
