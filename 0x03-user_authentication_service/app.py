a6f5bec48d09e3e09c9218ff79ba92a527efb42d
codam <damisco005@gmail.com>
2024-11-25 01:28:37 +0100

    implemented register usr route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,13 +2,29 @@
 """Simple flask app
 """
 import flask
+from auth import Auth
 
 app = flask.Flask(__name__)
+auth = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def root():
+    """root route"""
     return flask.jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def user_route():
+    """users route"""
+    req = flask.request
+    email = req.form.get('email')
+    password = req.form.get('password')
+    try:
+        auth.register_user(email=email, password=password)
+    except ValueError as v:
+        return flask.jsonify({"message": "email already registered"}), 400
+    return flask.jsonify({"email": email, "message": "user created"})
 
 
 if __name__ == "__main__":
